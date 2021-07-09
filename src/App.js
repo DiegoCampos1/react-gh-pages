8ee90a8a6896949ca3fd45070b7b5ef3d24fd69c
diegocampos1 <diegodiko05@gmail.com>
2021-07-09 17:18:54 -0300

    comitasso 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">Olá turma 11</div>;
+  return <div className="App">Olá turma 11, commitasso 2</div>;
 }
 
 export default App;
